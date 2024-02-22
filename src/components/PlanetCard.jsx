647f086e1740bea1a40c5354ad9637d88c9735fd
deepak1051 <lightyagamigoku@gmail.com>
2024-02-22 22:09:49 +0530

    fix show button

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -12,7 +12,7 @@ const PlanetCard = ({ planet }) => {
   }, []);
 
   return (
-    <div className="bg-white border flex flex-col  p-2 flex-1 shadow-md ">
+    <div className="bg-white border rounded-md flex flex-col  p-2 flex-1 shadow-md ">
       <div>
         <h2 className="font-bold text-lg">Name: {planet.name}</h2>
         <p>Climate: {planet.climate}</p>
@@ -20,10 +20,15 @@ const PlanetCard = ({ planet }) => {
         <p>Terrain:{planet.terrain}</p>
         <hr />
       </div>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 my-2">
         <div className="flex justify-between">
           <p className="text-lg font-bold">Residents List</p>
-          <button onClick={() => setShow((p) => !p)} className="underline">
+          <button
+            onClick={() => setShow((p) => !p)}
+            className={` p-1 rounded text-white  ${
+              show ? 'bg-red-500' : 'bg-green-500'
+            }`}
+          >
             {show ? 'hide list' : 'show list'}
           </button>
         </div>
@@ -43,26 +48,32 @@ const PlanetCard = ({ planet }) => {
           : null} */}
 
         {show ? (
-          <table className="table-auto border-collapse">
-            <thead>
-              <tr className="">
-                <th className=" text-left">Name</th>
-                <th className=" text-left">Height</th>
-                <th className=" text-left">Mass</th>
-                <th className=" text-left">Gender</th>
-              </tr>
-            </thead>
-            <tbody>
-              {residentList.map((r) => (
-                <tr key={r.name}>
-                  <td>{r.name}</td>
-                  <td>{r.height}</td>
-                  <td>{r.mass}</td>
-                  <td>{r.gender}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          residentList ? (
+            length > 0 ? (
+              <table className="table-auto border-collapse">
+                <thead>
+                  <tr className="">
+                    <th className=" text-left">Name</th>
+                    <th className=" text-left">Height</th>
+                    <th className=" text-left">Mass</th>
+                    <th className=" text-left">Gender</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {residentList.map((r) => (
+                    <tr key={r.name}>
+                      <td>{r.name}</td>
+                      <td>{r.height}</td>
+                      <td>{r.mass}</td>
+                      <td>{r.gender}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>This planet has no residents...</p>
+            )
+          ) : null
         ) : null}
       </div>
     </div>
